d0555feeb0abf55c639a76b874f1d966b8e1af11
Derickin06 <derikinho006@gmail.com>
2025-11-26 03:53:16 +0000

    teste

--- rha/CompareTo.java
+++ rha/CompareTo.java
@@ -2,5 +2,11 @@ public class CompareTo {
     protected String documento1;
     protected String documento2;
 
-    public 
+    public int compara(String documento1, String documento2) {
+       
+
+
+        
+        return 0;
+    }
 }
